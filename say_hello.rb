6567ff97743ff8422a862f88a354fef5634132d9
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-16 00:47:44 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 def say_hello(name)
-<<<<<<< HEAD
-  puts "Hello #{name}!"
+  puts "Hello #{name!}"
 end
 
 say_hello("Gabriela")
@@ -8,9 +7,4 @@ say_hello("Gabriela")
 def say_hello(name="Ruby programmer")
   puts "Hello #{name}!"
 end
-
-  puts "Hello #{name!}"
-end
-
-say_hello("Gabriela")
 
